4a16a1ee5a01e74b24c7dc8186ae575d6565f659
Bhoomi <122143468+Bhoomi1503@users.noreply.github.com>
2024-07-22 17:55:27 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function Incor1(){
+    document.querySelector("#option1").style.backgroundColor='red';
+    document.querySelector("#Answer1").style.backgroundColor='red';
+}
+
+function Incor2(){
+    document.querySelector("#option2").style.backgroundColor='red';
+    document.querySelector("#Answer2").style.backgroundColor='red';
+}
+
+function Incor3(){
+    document.querySelector("#option3").style.backgroundColor='red';
+    document.querySelector("#Answer3").style.backgroundColor='red';
+}
+
+function Correct(){
+    document.querySelector(".Opt").style.backgroundColor='green';
+    document.querySelector(".Opt").style.color='white';
+    document.querySelector(".Ans").style.backgroundColor='green';
+    document.querySelector(".Ans").style.color='white';
+}
